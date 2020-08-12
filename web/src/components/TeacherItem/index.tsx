fe97da6207eac2670a09c0e55ce352b712d31223
Luiz Serra <lserra.trade@gmail.com>
2020-08-11 23:42:09 -0300

    Finalizando Versão web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,34 +3,51 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
     return (
         <article className="teacher-item">
-                    <header>
-                        <img src="https://avatars0.githubusercontent.com/u/31904963?s=460&u=dddc6c1b0dfbf9d6d8d29346e7b9ded89b48f71e&v=4" alt="avatar" />
-                        <div>
-                            <strong>Luiz Serra</strong>
-                            <span>Programação avançada</span>
-                        </div>
-                    </header>
-                    <p>
-                        Entusiasta por modificar o mundo através dos códigos.
-                        <br />
-                        Aficcionado por códigos Mudando o mundo ao redor, um deploy de cada vez.
-                    </p>
-                    <footer>
-                        <p>
-                            Preço/hora
-                                    <strong>R$80,00</strong>
-                        </p>
-                        <button type="submit">
-                            <img src={whatsappIcon} alt="whatsapp" />
-                            Entrar em contato
-                        </button>
-                    </footer>
+            <header>
+                <img src={teacher.avatar} alt="avatar" />
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
+            <p>{teacher.bio}</p>
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ {teacher.cost}</strong>
+                </p>
+                <a target="_blank F" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
+                    <img src={whatsappIcon} alt="whatsapp" />
+                    Entrar em contato
+                </a>
+            </footer>
 
-                </article>
+        </article>
     );
 }
 export default TeacherItem;
